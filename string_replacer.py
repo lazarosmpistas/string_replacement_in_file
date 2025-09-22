02654f8ad41d577e35f771305069533539254ae1
lazarosmpistas <lazaros120@gmail.com>
2025-09-22 03:57:46 +0300

    init script commit

---
+++ string_replacer.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+
+def main(old_path, new_path, str_to_replace, new_str):
+    #filename = old_path.split('/')[-1]
+
+    old_p = Path(old_path)
+    if not old_p.is_file():
+        print('No such file in this location')
+        return
+
+    #filename = old_p.name
+    with open(old_p, "r") as f:
+        string = f.read()
+
+    string = string.replace(str_to_replace, new_str)
+
+    new_p = Path(new_path)
+    Path(new_p.parent).mkdir(parents=True, exist_ok=True)
+
+    with open(new_p, "w") as f:
+        f.write(string)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5 :
+        print("Usage: python string_replacer.py <old_filepath> <new_filepath> <string_to_change> <new_string>")
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
